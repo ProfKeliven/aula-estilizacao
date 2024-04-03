87ecf01983103479e8e919dfa50f338dc16fc3c6
ProfKeliven <professorakeliven@gmail.com>
2024-04-03 12:29:38 -0300

    finalização da estilizacao

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,11 +7,9 @@ export default function Banner(){
     return(
         <div className='banner container'>
             <div className="two-column content">
-                <div className="inner-content">
                     <TextBig label="Bem-vindo"/>
                     <p>Seja bem-vindo ao site.</p>
                     <Button label="Saiba mais" />
-                </div>
             </div>
             <div className="two-column content">
                 <img src="https://www.freeiconspng.com/thumbs/dog-png/dog-png-30.png" alt="Cachorro" srcset="" />
